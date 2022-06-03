31537a5a78591575e21377a3e139dd9453fc6ea6
zhongtaoaki <zhongtaodylan@163.com>
2022-06-03 16:03:51 +0900

    add repository

---
+++ examples/quick-start/Mall/src/main/java/net/samjna/mall/repository/UmsMenuRepository.java
@@ -0,0 +1,9 @@
+package net.samjna.mall.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import net.samjna.mall.model.UmsMenu;
+
+public interface UmsMenuRepository extends JpaRepository<UmsMenu,Long>{
+
+}
