e27cecad00939933cb9da44d28c2189f20208151
2013liliy <ly13253211668@gmail.com>
2022-09-05 17:46:48 +0900

    测试

---
+++ examples/quick-start/Mall/src/main/java/net/samjna/mall/model/CmsPrefrenceAreaProductRelation.java
@@ -0,0 +1,15 @@
+package net.samjna.mall.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class CmsPrefrenceAreaProductRelation {
+	
+	private int id;
+	private int prefrenceAreaId;
+	private int productId;
+	
+   
+}
